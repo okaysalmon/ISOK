f299194efe63a18ea3d5ec5ecee0905110d50dc8
Okay Salmon <okaysalmongames@gmail.com>
2025-11-24 08:02:36 +1100

    Update OS_IK.gd to version 1.0.6.0 with enhancements
    Updated code version to 1.0.6.0 and added AXIS enum. Improved bone rotation handling and adjusted alignment calculations.

--- addons/OSIK/OS_IK.gd
+++ addons/OSIK/OS_IK.gd
@@ -3,7 +3,9 @@
 extends SkeletonModifier3D
 class_name OSIK
 
-##Code Version 1.0.5.9##
+##Code Version 1.0.6.0##
+
+enum AXIS {X,Y,Z}
 
 ##[b][u]Will only make a diffrence if[/u][/b] the IK Target bone is the last in the [Skeleton3D]
 @export var tipBoneLenght:float = 0.1:
@@ -108,6 +110,8 @@ var WarrningTimmer:float = 5.0
 var WarrningTimmerslerpSpeed:float = 5.0
 var editorFocus:bool = true
 var boneCache : Dictionary
+
+var globalRot:Basis
 
 func _ready():
 	get_skeleton().child_order_changed.connect(update_OSIK_Array)
@@ -293,7 +297,8 @@ func _process_modification_with_delta(delta: float) -> void:
 								ParrentAngleForward = (IK_Look_Spots[inversI+1][0].origin - IK_Look_Spots[inversI][0].origin).normalized()
 							else:
 								var bone_idx: int = skeleton.find_bone(boneList[inversI])
-								ParrentAngleForward = ((skeleton.global_transform * skeleton.get_bone_global_pose(bone_idx-1)).origin -IK_Look_Spots[inversI][0].origin).normalized()
+								if bone_idx!=0:
+									ParrentAngleForward = ((skeleton.global_transform * skeleton.get_bone_global_pose(bone_idx-1)).origin -IK_Look_Spots[inversI][0].origin).normalized()
 								
 							IK_DirectionForward = clamp_directional_angle(ParrentAngleForward,IK_DirectionForward,Limits[inversI])
 							IK_Look_Spots[inversI-1][0].origin = IK_Look_Spots[inversI][0].origin - IK_DirectionForward*IK_Look_Spots[inversI][1]
@@ -317,7 +322,6 @@ func _process_modification_with_delta(delta: float) -> void:
 							
 			##The code below uses the position pointers created above to align each bone to the correct position along the IK chain.
 			## it also will slerp the basis towards the end goal
-			#$"../MeshInstance3D2".global_position = IK_Look_Spots[1][0].origin
 			for bone in boneList.size():
 				var bone_idx: int = skeleton.find_bone(boneList[boneList.size()-bone-1])
 				var pose: Transform3D = skeleton.global_transform * skeleton.get_bone_global_pose(bone_idx)
@@ -347,7 +351,12 @@ func _process_modification_with_delta(delta: float) -> void:
 					boneCurrentPoseArray[boneList.size()-bone-1].basis = boneCurrentPoseArray[boneList.size()-bone-1].basis.orthonormalized().slerp(local_pose.basis.orthonormalized(),slerpSpeed*1*delta).orthonormalized()
 				else:
 					boneCurrentPoseArray[boneList.size()-bone-1].basis = local_pose.basis.orthonormalized()
-				if bone != boneList.size()-1:
+				#Htis is where the roll lock will be applied once i have the code corected to fix y flip corection
+				#if bone_idx!=0:
+					#print(str(skeleton.get_bone_pose(bone_idx-1).basis) +" the index is " +str(boneCurrentPoseArray[boneList.size()-bone-1].basis))
+				#	boneCurrentPoseArray[boneList.size()-bone-1].basis = clamp_roll(skeleton.get_bone_pose(bone_idx-1).basis,boneCurrentPoseArray[boneList.size()-bone-1].basis,Limits[bone-1].maxRollDeg)
+				
+				if bone != boneList.size()-1 :
 					# Sets the new origin for the next bone based on the current bone’s basis Y direction.
 					boneCurrentPoseArray[boneList.size()-bone-2].origin = (boneCurrentPoseArray[boneList.size()-bone-1].origin+ (boneCurrentPoseArray[boneList.size()-bone-1].basis.orthonormalized().y.normalized() *(IK_Look_Spots[boneList.size()-bone-1][1]))/global_scale)
 				skeleton.set_bone_global_pose(bone_idx, boneCurrentPoseArray[boneList.size()-bone-1])
@@ -386,14 +395,26 @@ func _y_look_at_rel_to_globalTransform(from: Transform3D, target: Vector3, globa
 	var t_v: Vector3 = target - from.origin
 	var v_y: Vector3 = t_v.normalized()
 	
+	# making the out come for an ajusted that sutable for x alignment
+	var Test_Rest_Y= globalTransform.basis.y.normalized()
+	var rot_to_Aling = Test_Rest_Y.signed_angle_to(v_y,globalTransform.basis.z.normalized())
+	var X_adjust_globalTransform = globalTransform.basis.rotated(globalTransform.basis.z.normalized(),rot_to_Aling)
+	
+	# making the out come for an ajusted that sutable for z alignment
+	var ZTest_Rest_Y= globalTransform.basis.y.normalized()
+	var Zrot_to_Aling = Test_Rest_Y.signed_angle_to(v_y,globalTransform.basis.x.normalized())
+	var Z_adjust_globalTransform = globalTransform.basis.rotated(globalTransform.basis.x.normalized(),rot_to_Aling)
+	
+	#blend Based off if its closer to z or x
+	globalTransform = X_adjust_globalTransform.slerp(Z_adjust_globalTransform,abs(v_y.z))
+	
 	##Creating a fixed Roll relative to the rest pose's roll
 	var rest_z = globalTransform.basis.x.normalized()
 	var v_z: Vector3 = rest_z.cross(v_y).normalized()
 	var v_x: Vector3 = v_y.cross(v_z).normalized()
-	
 	from.basis = Basis(v_x, v_y, v_z)
-	
 	return from
+
 
 
 func clamp_directional_angle(reference: Vector3, target: Vector3, OSLimits: OSIK_Constraints) -> Vector3:
@@ -508,3 +529,52 @@ func return_true_if_update_required()->bool:
 func set_all_OSIK_for_update_required():
 	for node in allOSIK:
 		node.updateChainRequired = true
+
+
+func _detect_Axis_dif(basis1:Basis, basis2:Basis, AxisXYZ:AXIS = 1)->float:
+	# Get Euler angles
+	var euler1 = basis1.get_euler()
+	var euler2 = basis2.get_euler()
+
+	# Get the difference for the roll (z-axis)
+	#print()
+	var roll_diff = euler2.y - euler1.y
+	if AxisXYZ == 0:
+		roll_diff = euler2.x - euler1.x
+	elif AxisXYZ == 2:
+		roll_diff = euler2.z - euler1.z
+	# Normalize the angle to be within the range of -180 to +180 degrees
+	if roll_diff > PI:
+		roll_diff -= 2 * PI
+	elif roll_diff < -PI:
+		roll_diff += 2 * PI
+
+	# Convert to degrees to make it easier to use and understand
+	var roll_difference_deg = rad_to_deg(roll_diff)
+	#if roll_difference_deg!=0.0:
+		#print(roll_difference_deg)
+	return roll_difference_deg
+	
+
+## This function is still a work in progress. It currently has an issue caused by a Y-axis flip. Once the bone rotates past the 0-degree point on the Y axis, it calculates the roll incorrectly, which basically makes it spin like a drill.
+func clamp_roll(basis1:Basis, basis2:Basis, degree:float)->Basis:
+	#var AlimentRequired:float = basis2.y.normalized().signed_angle_to(basis1.y.normalized(), basis2.x.normalized())
+	#var AlinedBasis2:Basis = basis2.rotated(basis2.z.normalized(),AlimentRequired)
+	var roll_dif :float= _detect_Axis_dif(basis1,basis2)
+	var adjust_required :float = 0.0
+	#print("b1 "+str(basis1))
+	#print("b2 "+str(basis2))
+	#print(degree)
+	if roll_dif > degree:
+		adjust_required = (roll_dif - degree)*-1
+	elif roll_dif < -degree:
+		adjust_required = (roll_dif + degree)*-1
+	if adjust_required != 0.0:
+		#print(adjust_required)
+		#var rotVector = basis2.y.normalized()
+		#var rotAmount = adjust_required
+		#if rotVector.y <0:
+		#	rotAmount = -rotAmount
+		#print(rotVector)
+		basis2 = basis2.rotated(basis2.y.normalized(),deg_to_rad((adjust_required)))
+	return basis2
